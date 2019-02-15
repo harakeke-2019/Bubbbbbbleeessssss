03b2dc2dda68c2eaf60319d3d69f9fd5d573b164
Micah Nathaniel Titus Peautagi <micahpeautagi@gmail.com>
2019-02-15 15:59:28 +1300

    update css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Route} from 'react-router-dom'
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 import HomePage from './HomePage'
 import Music from './Music'
@@ -11,13 +12,18 @@ import Loading from './Loading'
 class App extends Component {
 render() {
   return (
-    <div>
+    <div class="ui container">
+    <div className="header">
+    <h1 className>BEST API AGGREGATOR EVER</h1>
+    </div>
+    <div class="ui container">
     
     <Route exact path = '/' component = {HomePage} />
     <Route path = '/profile' component = {Profile}/>
     <Route path = '/music' component = {Music}/>
     <Route path = '/loading' component = {Loading}/>
     {/* <Route path = '/rating' component = {Rating} />  */}
+    </div>
     </div>
   )
 }
