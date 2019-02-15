7d62c8b089ed5d4693872b25428135701581c5c6
Micah Nathaniel Titus Peautagi <micahpeautagi@gmail.com>
2019-02-15 14:17:59 +1300

    Loading music to loading page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Route} from 'react-router-dom'
 import HomePage from './HomePage'
 import Music from './Music'
 import Profile from './Profile'
+import Loading from './Loading'
 
 
 
@@ -15,6 +16,7 @@ render() {
     <Route exact path = '/' component = {HomePage} />
     <Route path = '/profile' component = {Profile}/>
     <Route path = '/music' component = {Music}/>
+    <Route path = '/loading' component = {Loading}/>
     {/* <Route path = '/rating' component = {Rating} />  */}
     </div>
   )
--- src/Loading.js
+++ src/Loading.js
@@ -1,16 +1,53 @@
 import React from 'react'
+import sound from './sounds/time-travel.mp3'
 
 class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        'play': false
+      };
+
+      this.url = sound
+      this.audio = new Audio(this.url);
+  }
+  togglePlay = () => {
+    this.setState({'play': !this.state.play}, () => {
+      if(this.state.play) {
+        this.audio.play()
+      } else {
+        this.audio.pause();
+      }
+    });
+  }
+  componentWillUnmount () {
+    this.audio.pause();
+  }
+
+
+
 
   render () {
 
 
     return (
       <div>
-        <h1>Loading infinity and beyond skuxx</h1>
-        <div class="ui active centered inline loader">
+        <h2>Loading your search results...</h2>
+        <div class="ui placeholder segment">
+        {/* <div class="ui big active text loader">To infinity and beyond
+        </div> */}
+        <div class="ui active big progress">
+        <div class="bar"></div>
+        <div class="label">To infinity and beyond</div>
         </div>
+        </div>
+        <button onClick={this.togglePlay} class="big ui right labeled icon button">
+        <i class="left arrow icon"></i>
+        Go back in time
+        </button>
       </div>
     )
   }
 }
+
+export default Loading 
