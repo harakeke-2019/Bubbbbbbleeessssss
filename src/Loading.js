d82c7e2fd6483a5e834bf34ec1293a2e8240d5c4
Jason James Simpson <jasonjamessimpson@gmail.com>
2019-02-15 13:30:41 +1300

    Setup Loading component

---
+++ src/Loading.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Loading extends React.Component {
+
+  render () {
+
+
+    return (
+      <div>
+        <h1>Loading infinity and beyond skuxx</h1>
+        <div class="ui active centered inline loader">
+        </div>
+      </div>
+    )
+  }
+}
